b2b6b7440e0b2a31acbb7a213a9d3aa272f8572f
Gianmarco Terenzi <seld666@gmail.com>
2021-05-20 15:44:17 +0200

    Create checkATagHref.py

---
+++ checkATagHref.py
@@ -0,0 +1,33 @@
+import requests
+from selenium import webdriver
+
+driver=webdriver.Chrome(executable_path='C:\Program Files (x86)\chromedriver.exe')
+
+def checkATagHrefs(page):
+    driver.get(page)
+    atags = driver.find_elements_by_tag_name('a')
+    hrefvalues = list(map(lambda atag: atag.get_attribute('href'), atags))
+
+    links = []
+    linksCount = []
+
+    for href in hrefvalues:
+        if href not in links:
+            links += [href]
+            linksCount += [[href,hrefvalues.count(href)]]
+    print("--------------------------")
+    print("testing:",page)
+    for link in linksCount:
+        try:
+            r = requests.head(link[0])
+            if r.status_code >= 400:
+                print(link[0], r.status_code, " x", link[1])
+        except:
+            print(link)
+    
+
+pages = ["https://www.google.it/","https://www.python.org/"]
+
+for page in pages:
+    checkATagHrefs(page)
+print("---END---")
